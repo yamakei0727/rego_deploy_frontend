420228de53d71fc2fe4a183595d177c758e96db6
yamakei0727 <yamakei0727@gmail.com>
2025-02-17 21:19:55 +0900

    legolessonYML4

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'standalone',
-    env: {
-      API_ENDPOINT: process.env.API_ENDPOINT,
-    }
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {};
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
+export default nextConfig;
